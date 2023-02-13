a04890700f244e195f4ec5a52ba3e8b0c12727e5
Artur <alturik@mail.ru>
2023-02-13 20:31:41 +0300

    Made a task with SQL query.

---
+++ SQL_Task.sql
@@ -0,0 +1,5 @@
+﻿Select Products.Name, Categories.Name
+From Products Left Join ProductCategory
+On Products.Id = ProductCategory.ProductId 
+Left Join Categories
+On Categories.Id = ProductCategory.CategoryId
